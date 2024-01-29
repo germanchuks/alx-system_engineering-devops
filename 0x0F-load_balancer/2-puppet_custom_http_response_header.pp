a5bfac029c77634b7caa36bec0f806b4953f50f4
Daniel German <danielchuksgerman@gmail.com>
2024-01-29 06:58:16 -0800

    Install your load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+# Automate the creation of custom HTTP header response using Puppet
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html':
+  ensure => directory,
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => '
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      add_header X-Served-By $hostname;
+      root /var/www/html;
+      index index.html index.htm;
+
+      location /redirect_me {
+        return 301 https//youtube.com/;
+      }
+
+      error_page 404 /404.html;
+      location /404 {
+        root /var/www/html;
+        internal;
+      }
+    }
+  ',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => File['/etc/nginx/sites-available/default'],
+  enable  => true,
+}

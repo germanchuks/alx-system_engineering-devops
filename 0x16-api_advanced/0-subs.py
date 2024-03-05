855e96bd15aff041435129083c8cbb44700d5d0e
Daniel German <danielchuksgerman@gmail.com>
2024-03-05 13:42:36 -0800

    How many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that queries the Reddit API and returns the number of subscribers
+for a given subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers={"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0

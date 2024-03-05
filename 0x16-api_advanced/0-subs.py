7ac3411365c98eb8a0b3d149a1233e8ad181a557
Daniel German <danielchuksgerman@gmail.com>
2024-03-05 14:37:02 -0800

    Fix task 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers={"User-Agent": "My-User-Agent"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {"User-Agent": "My-User-Agent"}
+
+    response = requests.get(url,
+                            headers=headers,
+                            allow_redirects=False)
+    if response.status_code >= 300:
         return 0
+    else:
+        data = response.json().get("data")
+        subscribers = data.get("subscribers")
+        return subscribers
